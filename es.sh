5eeda59d054902eec3e06b3ddbc0bfa7c7d097a5
Julien Danh <danh.julien@gmail.com>
2025-07-28 16:41:46 +0200

    add es-config-app

--- es.sh
+++ es.sh
@@ -5,5 +5,6 @@ kubectl apply -f https://download.elastic.co/downloads/eck/3.0.0/crds.yaml
 kubectl apply -f https://download.elastic.co/downloads/eck/3.0.0/operator.yaml
 
 kubectl apply -f elasticsearch/argocd-app.yaml -n argocd
+kubectl apply -f es-config-app/argocd-app.yaml -n argocd
 
 echo "Elasticsearch ArgoCD app deployed!"

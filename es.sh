1e82c734f0ab9848973df39c9596f0abb1311583
Julien Danh <danh.julien@gmail.com>
2025-07-28 16:19:35 +0200

    fix es.sh

--- es.sh
+++ es.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 set -e
 
-kubectl create -f https://download.elastic.co/downloads/eck/3.2.0/crds.yaml
+kubectl apply -f https://download.elastic.co/downloads/eck/3.0.0/crds.yaml
+kubectl apply -f https://download.elastic.co/downloads/eck/3.0.0/operator.yaml
 
-kubectl apply -f https://download.elastic.co/downloads/eck/3.2.0/operator.yaml
-
-# Deploy the ArgoCD Application for Elasticsearch
 kubectl apply -f elasticsearch/argocd-app.yaml -n argocd
 
 echo "Elasticsearch ArgoCD app deployed!"

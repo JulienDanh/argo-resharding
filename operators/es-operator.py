1d496fe0e818705e05426d14e355b9bba06db24f
Julien Danh <danh.julien@gmail.com>
2025-07-29 16:18:11 +0200

    wip

--- operators/es-operator.py
+++ operators/es-operator.py
@@ -10,15 +10,25 @@ PLURAL = "reshardings"
 
 api = kubernetes.client.CustomObjectsApi()
 
+
+def reshard(sourceIndex: str, targetIndex: str):
+    print(f"Resharding from {sourceIndex} to {targetIndex}")
+    sleep(5)  # Simulate resharding process
+
+
 while True:
     try:
         reshardings = api.list_namespaced_custom_object(
             group=GROUP, version=VERSION, namespace=NAMESPACE, plural=PLURAL
         )
-        print(reshardings)
         for item in reshardings.get("items", []):
             name = item["metadata"]["name"]
             spec = item.get("spec", {})
+            sourceIndex = spec.get("sourceIndex")
+            targetIndex = spec.get("targetIndex")
+
+            reshard(sourceIndex, targetIndex)
+
             if spec.get("status") != "DONE":
                 body = {"spec": dict(spec)}
                 body["spec"]["status"] = "DONE"

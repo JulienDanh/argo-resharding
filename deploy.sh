f2dac4b223ab460721d4d04a766d33552be89f00
Julien Danh <danh.julien@gmail.com>
2025-07-29 10:10:16 +0200

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ set -e
 kubectl apply -f https://download.elastic.co/downloads/eck/3.0.0/crds.yaml
 kubectl apply -f https://download.elastic.co/downloads/eck/3.0.0/operator.yaml
 
-kubectl apply -f elasticsearch/argocd-app.yaml -n argocd
-kubectl apply -f es-config-app/argocd-app.yaml -n argocd
+kubectl apply -f argocd-apps/es-app.yaml -n argocd
+kubectl apply -f argocd-apps/es-config-app.yaml -n argocd
 
 echo "Elasticsearch ArgoCD app deployed!"
